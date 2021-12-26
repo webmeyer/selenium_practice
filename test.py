33944434fa1c6843b8f2baf09a3c702bb7d0639f
webmeyer <meyerwork22@gmail.com>
2021-12-26 23:25:38 +0000

    Add new test files

--- test.py
+++ test.py
@@ -1,35 +1,52 @@
-import time
-import math
+import pytest
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
+import time
+import math
 
 
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+links_url = [
+    'https://stepik.org/lesson/236895/step/1',
+    'https://stepik.org/lesson/236896/step/1',
+    'https://stepik.org/lesson/236897/step/1',
+    'https://stepik.org/lesson/236898/step/1',
+    'https://stepik.org/lesson/236899/step/1',
+    'https://stepik.org/lesson/236903/step/1',
+    'https://stepik.org/lesson/236904/step/1',
+    'https://stepik.org/lesson/236905/step/1']
 
-try:
+@pytest.fixture(scope="class")
+def browser():
+    print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
-    # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-    price = WebDriverWait(browser, 20).until(
-        EC.text_to_be_present_in_element((By.ID, "price"), '100')
-    )
-
-    button = browser.find_element(By.ID, 'book')
-    button.click()
-
-    get_x = browser.find_element(By.ID, 'input_value').text
-    result = calc(get_x)
-
-    answer = browser.find_element(By.ID, 'answer')
-    answer.send_keys(result)
-
-    button_submit = browser.find_element(By.ID, 'solve')
-    button_submit.click()
-    time.sleep(5)
-
-finally:
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
     browser.quit()
+
+
+@pytest.mark.parametrize('links', links_url)
+class TestMainPage1:
+    """ Заполнение форм и поиск ответа """
+    def test_parametrize(self, browser, links):
+        wait = WebDriverWait(browser, 10)
+        print("start test_parametrize")
+        browser.get(links)
+        textarea = browser.find_element(By.XPATH, '//textarea')
+        textarea.send_keys(str(math.log(int(time.time()) - 14720.6)))
+        try:
+            wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'submit-submission')))
+            button_submit = browser.find_element(By.CLASS_NAME, 'submit-submission')
+            button_submit.click()
+            wait.until(EC.presence_of_element_located((By.XPATH, '//pre[@class="smart-hints__hint"]')))
+        except NameError as nameErr:
+            print('Ошибка парсинга ответа', nameErr)
+
+        answer_message = browser.find_element(By.XPATH, '//pre[@class="smart-hints__hint"]').text
+        print(answer_message)
+        assert answer_message == 'Correct!', 'FAILED'
+
+        print("finish test_parametrize")
+
