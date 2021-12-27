4276b57ce7e4d0af98200db21ee8a62a81af4ea1
webmeyer <meyerwork22@gmail.com>
2021-12-27 00:58:33 +0000

    refactored

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
+import time
+import math
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
-import math
 
 
 links_url = [
@@ -17,26 +17,19 @@ links_url = [
     'https://stepik.org/lesson/236904/step/1',
     'https://stepik.org/lesson/236905/step/1']
 
-@pytest.fixture(scope="class")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(5)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
 
 @pytest.mark.parametrize('links', links_url)
-class TestMainPage1:
+class TestPageResults:
     """ Заполнение форм и поиск ответа """
     def test_parametrize(self, browser, links):
         wait = WebDriverWait(browser, 10)
         print("start test_parametrize")
         browser.get(links)
-        textarea = browser.find_element(By.XPATH, '//textarea')
-        textarea.send_keys(str(math.log(int(time.time()) - 14720.6)))
+        browser.implicitly_wait(3)
         try:
+            wait.until(EC.presence_of_element_located((By.XPATH, '//textarea')))
+            textarea = browser.find_element(By.XPATH, '//textarea')
+            textarea.send_keys(str(math.log(int(time.time()) - 14720.6)))
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'submit-submission')))
             button_submit = browser.find_element(By.CLASS_NAME, 'submit-submission')
             button_submit.click()
@@ -46,7 +39,7 @@ class TestMainPage1:
 
         answer_message = browser.find_element(By.XPATH, '//pre[@class="smart-hints__hint"]').text
         print(answer_message)
-        assert answer_message == 'Correct!', 'FAILED'
+        assert answer_message == 'Correct!', 'Неверный код!'
 
         print("finish test_parametrize")
 
